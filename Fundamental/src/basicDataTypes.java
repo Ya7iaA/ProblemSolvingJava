da3a334a850c159469ed541409638cb42ad8f084
Ya7iaA <yahiaahmed.y72a@gmail.com>
2025-05-26 22:32:55 +0300

    Basic Data Types Update

--- Fundamental/src/basicDataTypes.java
+++ Fundamental/src/basicDataTypes.java
@@ -4,13 +4,13 @@ public class BasicDataTypes {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        int intValue = s.nextInt();
+        int integerValue = s.nextInt();
         long longValue = s.nextLong();
         char charValue = s.next().charAt(0);
         float floatValue = s.nextFloat();
         double doubleValue = s.nextDouble();
 
-        System.out.println(intValue);
+        System.out.println(integerValue);
         System.out.println(longValue);
         System.out.println(charValue);
         System.out.println(floatValue);
