59813927d10d25034159ef1205ca230b7a8de439
Ya7iaA <yahiaahmed.y72a@gmail.com>
2025-07-14 21:22:44 +0300

    SimpleCalculator updated

--- Fundamental/src/SimpleCalculato.java
+++ Fundamental/src/SimpleCalculato.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SimpleCalculator {
+public class SimpleCalculato {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
