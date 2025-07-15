e784c5a385faa662138da5e0d4ead9d7d72bf7c6
Ya7iaA <yahiaahmed.y72a@gmail.com>
2025-07-16 01:55:20 +0300

    Comparison solved

--- Fundamental/src/Comparison.java
+++ Fundamental/src/Comparison.java
@@ -4,7 +4,29 @@ public class Comparison {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        
+        int firstNumber = s.nextInt();
+        String inputOperator = s.next();
+        int secondNumber = s.nextInt();
+
+        if (inputOperator == "=") {
+            if (firstNumber == secondNumber) {
+                System.out.println("Right");
+            } else {
+                System.out.println("Wrong");
+            }
+        } else if (inputOperator == ">") {
+            if (firstNumber > secondNumber) {
+                System.out.println("Right");
+            } else {
+                System.out.println("Wrong");
+            }
+        } else if (inputOperator == "<") {
+            if (firstNumber > secondNumber) {
+                System.out.println("Right");
+            } else {
+                System.out.println("Wrong");
+            }
+        }
 
         s.close();
     }
